b4fc09e52f0cd465545e51fe46c2ecc0421b56ea
Rodrigo Pernambuco <rodrigoperera1@hotmail.com>
2023-03-28 23:01:30 -0300

    testes para estudo de tipo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        // classe String sempre representa texto
+        
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // Se começar com 0 o tipo não vai ser reconhecido - sujeito a alteração da tipagem
+        long cpf = 98765432109L; // Se começar com 0 o tipo não vai ser reconhecido - sujeito a alteração da tipagem - o valor da variavel tem que terminal com "l" ou "L"
+        float pi = 3.14F; // Igual ao long, o valor da variavel tem que terminar com "f" ou "F"
+        double salario = 1275.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14; //variavel nunca pode ser atribuida com novo valor e sempre tem que estar em CAIXA_ALTA com underline
     }
 }
